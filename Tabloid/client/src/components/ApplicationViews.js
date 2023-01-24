7d441565692addc5f4fbfbcc5b04eea435ca7804
Renee Doll <renee@reneedoll.com>
2023-01-24 13:41:01 -0600

    Made Tags admin only

--- Tabloid/client/src/components/ApplicationViews.js
+++ Tabloid/client/src/components/ApplicationViews.js
@@ -17,7 +17,17 @@ export default function ApplicationViews({ isLoggedIn, role }) {
           />
           <Route path="login" element={<Login />} />
           <Route path="register" element={<Register />} />
-          <Route path="tags" element={<Tags />} />
+          <Route path="tags" >
+          <Route index
+          element={
+            isLoggedIn
+              ? role === "Admin"
+                ? <Tags />
+                : <Navigate to="/tags" />
+              : <Navigate to="/login" />
+          }
+          />
+          </Route>
 
           <Route path="categories">
             <Route index
